87cf8bca3f267e0104226f0992d4c77d0aa46d3b
unknown <shinup222@naver.com>
2013-01-19 00:15:25 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name/:name2', function ($name, $name2) {
+    echo "Hello, $name, $name2";
+});
+// POST route
+$app->post('/post', function () {
+	$var1 = $app->request()->params('f1');
+    echo "This is a POST route, $var1";
+});
+// PUT route
+$app->put('/put', function () {
+    echo 'This is a PUT route';
+});
+
+// DELETE route
+$app->delete('/delete', function () {
+    echo 'This is a DELETE route';
+});
+$app->run();
